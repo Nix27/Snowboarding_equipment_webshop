1713cc8bf0a161a38db4fc504563f6b09a6a730a
Nix27 <nikolazecic2468@gmail.com>
2023-09-07 23:35:55 +0200

    Started customer product page

---
+++ Snowboarding_equipment_webshop/Areas/Customer/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Snowboarding_equipment_webshop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProductsController : Controller
+    {
+        public IActionResult OurProducts()
+        {
+            return View();
+        }
+    }
+}
