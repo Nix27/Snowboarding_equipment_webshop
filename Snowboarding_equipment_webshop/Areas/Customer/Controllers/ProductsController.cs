a8b8860344baa8eaca4fbb5e9d8a860d27ba070c
Nix27 <nikolazecic2468@gmail.com>
2023-09-09 00:00:54 +0200

    Finished ui for filtering and products, added a partail view for products and started pagination

--- Snowboarding_equipment_webshop/Areas/Customer/Controllers/ProductsController.cs
+++ Snowboarding_equipment_webshop/Areas/Customer/Controllers/ProductsController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Snowboarding_equipment_webshop.ViewModels;
 
 namespace Snowboarding_equipment_webshop.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ProductsController : Controller
     {
-        public IActionResult OurProducts()
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IMediator mediator, IMapper mapper, ILogger<ProductsController> logger)
         {
+            _mediator = mediator;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public IActionResult OurProducts(PagedProductsRequestVM productsRequest)
+        {
+
             return View();
         }
     }
---
+++ Snowboarding_equipment_webshop/ViewModels/PagedProductsRequestVM.cs
@@ -0,0 +1,15 @@
+﻿using NuGet.Protocol.Core.Types;
+
+namespace Snowboarding_equipment_webshop.ViewModels
+{
+    public class PagedProductsRequestVM
+    {
+        public int Size { get; set; }
+        public int Page { get; set; }
+        public string SortBy { get; set; } = "Name";
+        public IList<string>? Categories { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+        public string? SearchTerm { get; set; }
+    }
+}
