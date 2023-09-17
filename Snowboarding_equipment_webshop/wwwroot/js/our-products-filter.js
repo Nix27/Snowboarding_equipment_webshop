6340aedc87ce470335615b626ed1d4abea86de19
Nix27 <nikolazecic2468@gmail.com>
2023-09-17 18:02:12 +0200

    Home, products and product details pages are fully responsive, I need to make shopping cart and order summary also responsive.

---
+++ Snowboarding_equipment_webshop/wwwroot/js/our-products-filter.js
@@ -0,0 +1,11 @@
+﻿$('#filter-btn').on('click', function () {
+    $('.mobile-filter-container').css('left', '0');
+    $('#close-filter-btn').css('display', 'block');
+    $('body').css('overflow', 'hidden');
+});
+
+$('#close-filter-btn').on('click', function () {
+    $('.mobile-filter-container').css('left', '-2000px');
+    $('#close-filter-btn').css('display', 'none');
+    $('body').css('overflow', 'auto');
+});
